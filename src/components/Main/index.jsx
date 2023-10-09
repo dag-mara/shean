5277ec99a4590f57011077875ac1cf30eb9c93e4
Dagmar Suchá <sucha.dagmar@gmail.com>
2023-10-09 15:35:26 +0200

    format document

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -13,12 +13,12 @@ export const Main = () => {
 
     function openModal() {
         setIsOpen(true);
-      }
+    }
 
     function closeModal() {
-    setIsOpen(false);
-    setEmail('');
-    document.querySelector('#email').style.backgroundImage={emailIcon};
+        setIsOpen(false);
+        setEmail('');
+        document.querySelector('#email').style.backgroundImage = { emailIcon };
     }
 
     Modal.setAppElement('#app');
@@ -29,48 +29,48 @@ export const Main = () => {
     }
 
     useEffect(() => {
-        setCode(Math.random().toString(36).substring(2,10).toUpperCase());
+        setCode(Math.random().toString(36).substring(2, 10).toUpperCase());
     }, []);
 
     const onEmailChange = (e) => {
         setEmail(e.target.value);
-        document.querySelector('#email').style.backgroundImage='none';
+        document.querySelector('#email').style.backgroundImage = 'none';
     }
 
-    return(
+    return (
         <>
-        <div className="container">
-            <div className="left">
-                <img className="header-image" src={headerImage} alt="Oslavte s námi narozeniny" role="banner"/>
-                <p className='content' role="main"><strong>Slavíme 15 let! </strong>Vyzvedněte si od nás dárek, který vám vykouzlí úsměv na tváři. Po zadání e-mailu se vám ozveme a domluvíme se společně, jak váš dárek budete chtít využít.
-                </p>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="">
-                        Váš dárkový kód je
-                        <input type="text" placeholder={code} name="code" id="code"/>
-                    </label>
-                    
-                    <label htmlFor="">
-                        Vyplňte e-mail
-                        <input type="email" id="email" name="email" onChange={onEmailChange} required placeholder='vas@email.cz'/>
-                    </label>
-                   
-                    <button type="submit">ZÍSKAT DÁREK</button>
-                    <p className='gdpr-text'>Ochrana osobních údajů je pro nás prioritou.<br />
-                    Více informací jak zacházíme s vašimi daty, najdete v <a href="">tomto dokumentu</a>
+            <div className="container">
+                <div className="left">
+                    <img className="header-image" src={headerImage} alt="Oslavte s námi narozeniny" role="banner" />
+                    <p className='content' role="main"><strong>Slavíme 15 let! </strong>Vyzvedněte si od nás dárek, který vám vykouzlí úsměv na tváři. Po zadání e-mailu se vám ozveme a domluvíme se společně, jak váš dárek budete chtít využít.
                     </p>
-                </form>
-               <div className="sheep-wrapper">
-                    <img className="sheep-small" src={sheep} alt="ovce" />
-               </div>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="">
+                            Váš dárkový kód je
+                            <input type="text" placeholder={code} name="code" id="code" />
+                        </label>
+
+                        <label htmlFor="">
+                            Vyplňte e-mail
+                            <input type="email" id="email" name="email" onChange={onEmailChange} required placeholder='vas@email.cz' />
+                        </label>
+
+                        <button type="submit" role="button">ZÍSKAT DÁREK</button>
+                        <p className='gdpr-text'>Ochrana osobních údajů je pro nás prioritou.<br />
+                            Více informací jak zacházíme s vašimi daty, najdete v <a href="https://www.shean.cz/zpracovani-osobnich-udaju/">tomto dokumentu</a>
+                        </p>
+                    </form>
+                    <div className="sheep-wrapper">
+                        <img className="sheep-small" src={sheep} alt="ovce" />
+                    </div>
+                </div>
+
+                <div className="right">
+                    <img className="sheep" src={sheep} alt="ovce" />
+                </div>
             </div>
-           
-            <div className="right">
-                <img className="sheep" src={sheep} alt="ovce" />
-            </div>
-        </div>
-    
-        <ModalWindow modalIsOpen={modalIsOpen} closeModal={closeModal} email={email} code={code}/>
+
+            <ModalWindow modalIsOpen={modalIsOpen} closeModal={closeModal} email={email} code={code} />
         </>
     )
 }
